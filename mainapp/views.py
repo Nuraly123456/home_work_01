fec7b9f7f9f5e8424db9c2a510cddc15ca3b1bf8
Nuraly123456 <iman.zhsp@mail.ru>
2025-04-29 18:25:18 +0500

    lesson_04: context

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,26 @@
+# mainapp/views.py
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    title = 'Главная страница'
+
+    context = {
+        'title': title,
+    }
+
+    return render(request, 'index.html', context)
+
+def contacts(request):
+    return  render(request, 'contacts.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def products(request):
+    return render(request, 'products.html')
+
+def product(request):
+    return render(request, 'product.html')
+
+def base(request):
+    return render(request, 'base.html')
