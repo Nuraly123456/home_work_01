15e851069aca9f30e8e5c2ffeaa35c6e66094e1a
Nuraly123456 <iman.zhsp@mail.ru>
2025-05-08 18:30:08 +0500

    работа с дпнными

--- mainapp/models.py
+++ mainapp/models.py
@@ -74,3 +74,4 @@ class Product(models.Model):
     class Meta:
         verbose_name = 'Продукт'
         verbose_name_plural = 'Продукты'
+
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,11 +1,15 @@
 # mainapp/views.py
 from django.shortcuts import render
+from .models import Product
 
 def index(request):
     title = 'Главная страница'
 
+    prods = Product.objects.all()[:3]
+
     context = {
         'title': title,
+        'products': prods,
     }
 
     return render(request, 'index.html', context)
