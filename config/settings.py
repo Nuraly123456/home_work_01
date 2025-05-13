5b89daaa26383ef412264dd40d1fab393a7a446a
Nuraly123456 <iman.zhsp@mail.ru>
2025-05-13 18:55:38 +0500

    Сабақ 7 бойынша өзгерістер

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'mainapp',
+    'authapp',
+
 ]
 
 MIDDLEWARE = [
@@ -52,6 +54,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'config.urls'
+AUTH_USER_MODEL = 'authapp.ShopUser'
 
 TEMPLATES = [
     {
