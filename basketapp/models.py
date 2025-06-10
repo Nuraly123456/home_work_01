56e5bcd42cdb5611640ba7926e13706280fc4fbf
Nuraly123456 <iman.zhsp@mail.ru>
2025-06-10 19:10:19 +0500

    lesson_13: basketapp

--- basketapp/models.py
+++ basketapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from mainapp.models import Product
+from config import settings
 
-# Create your models here.
+class Basket(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='basket',
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+    )
+
+    quantity = models.PositiveIntegerField(
+        verbose_name='Количество',
+        default=0,
+    )
+
+    add_datetime = models.DateTimeField(
+        verbose_name='Время',
+        auto_now_add=True,
+    )
--- basketapp/views.py
+++ basketapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect, JsonResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from basketapp.models import Basket
+from mainapp.models import Product
+
+
+
+def basket(request):
+    if request.user.is_authenticated:
+        basket_ = Basket.objects.filter(user=request.user)
+        context = {
+            'basket': basket_,
+        }
+        return render(request, 'basketapp/basket.html', context)
+    return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
+
+def basket_add(request, product_id):
+    product_ = get_object_or_404(Product, id=product_id)
+
+    # Try to get existing basket item or create a new one
+    basket_item, created = Basket.objects.get_or_create(
+        user=request.user,
+        product=product_,
+        defaults={'quantity': 1}
+    )
+
+    if not created:
+        basket_item.quantity += 1
+        basket_item.save()
+
+    return JsonResponse({'status': 'success'})
+def basket_remove(request, pk):
+    return render(request, 'basketapp/basket.html')
